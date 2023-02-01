60c5a2df55fa831753239c77bf0ea16e17854083
ZtrainWilliams <ztrain1224@163.com>
2023-02-01 15:39:53 +0800

    fix: isInVSCode-openInCodeSandBox-todo

--- src/shared/codesandbox/index.ts
+++ src/shared/codesandbox/index.ts
@@ -1,14 +1,11 @@
 import { getParameters } from 'codesandbox/lib/api/define';
 import { message } from 'antd';
-import { isInVSCode } from '../vscode';
+import { fetchInVSCode, isInVSCode } from '../vscode';
 import indexHtml from './indexHtml';
 import MainJs from './mainJs';
 import genJson from './packageJson';
 
-export default function openOnCodeSandbox({
-  componentCode = '',
-  boxDependencies = {},
-}) {
+export default function openOnCodeSandbox({ componentCode = '', boxDependencies = {} }) {
   const parameters = getParameters({
     files: {
       'package.json': {
@@ -36,17 +33,15 @@ export default function openOnCodeSandbox({
     },
   });
 
-  const actionUrl =
-    'https://codesandbox.io/api/v1/sandboxes/define?file=/App.tsx';
+  const actionUrl = 'https://codesandbox.io/api/v1/sandboxes/define?file=/App.tsx';
 
   if (isInVSCode) {
-    /*   fetchInVSCode(
-      { url: actionUrl, data: { parameters } },
-      'openInCodeSandBox',
-    ); */
-    message.warning(
-      'VSCode环境下暂不支持CodeSandbox跳转预览，如需使用，请使用网页版 http://codegen.leekhub.com',
-    );
+    // fetchInVSCode({ url: actionUrl, data: { parameters } }, 'openInCodeSandBox').then((res: any) => {
+    //   console.log('res', res);
+    //   window.location.href = 'about:blank';
+    //   document.write(res);
+    // });
+    message.warning('VSCode环境下暂不支持CodeSandbox跳转预览，如需使用，请使用网页版 http://codegen.leekhub.com');
     return;
   }
   const form = document.createElement('form');
