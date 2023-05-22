dd4ab23c2a77611bf2143e983fd3bfb3516c45f7
ZtrainWilliams <ztrain1224@163.com>
2023-05-22 15:42:32 +0800

    chore: proxy

--- .umirc.ts
+++ .umirc.ts
@@ -12,13 +12,18 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  proxy: {
+    // 'http://youip/swagger-resources': {
+    //   target: 'http://youip/swagger-resources',
+    //   changeOrigin: true,
+    // },
+  },
   history: {
     // 默认的类型是 `browser`，但是由于 vscode webview 环境不存在浏览器路由，改成 `memory` 和 `hash` 都可以（这里坑了好久）
     type: 'memory',
   },
   // 兼容VSCode开发
-  outputPath:
-    process.env.APP_TYPE === 'site' ? 'dist' : '../../templates/codegen',
+  outputPath: process.env.APP_TYPE === 'site' ? 'dist' : '../../templates/codegen',
   routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
 });
